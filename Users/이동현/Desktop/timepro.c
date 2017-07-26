b15e5db325bd81fe513bf197381aa30f8bdff5a6
donghyeon2 <tsvz1234@gmail.com>
2017-07-26 20:10:50 +0900

    bug fix

--- Users/이동현/Desktop/timepro.c
+++ Users/이동현/Desktop/timepro.c
@@ -1,4 +1,6 @@
-int __fastcall sub_20948(int a1)
+//sub_2xxxxx start //
+
+char* basic_setting_ex_hostname(int a1)
 {
   int v1; // r4@1
   int v2; // r0@1
@@ -50,4 +52,44 @@ int __fastcall sub_20948(int a1)
   printf("</td></tr>");
   printf("</table>");
   return printf("</form>");
+}
+
+char* sub_20C6C(int a1)
+{
+  int v1; // r6@1
+  int v2; // r0@1
+
+  v1 = a1;
+  sub_1CF0C();
+  sub_15988("sysconf", "misc");
+  printf("<style>");
+  printf("form { margin: 0; }");
+  printf("input[type='text'] { border: 1px solid #CCC; }");
+  printf("input[type='radio'], input[type='checkbox'] { vertical-align: sub; }");
+  printf("</style>");
+  sub_20948(v1);
+  sub_1CCC8("apply_mask", sub_15A94(31));
+  sub_1CCC8("reboot_mask", sub_15A94(1024));
+  printf("<style>");
+  printf("#%s p { margin: 5px 0 5px 10px; text-align: left; }", "configmgmt-");
+  printf("</style>");
+  printf("<DIV id=\"%s\" style=\"display:none;\">", "configmgmt-");
+  puts("<DIV style=\"position:absolute; left:0px; top:0px; width:642px; height:535px; z-index:10; background-color:#ffffff;
+    opacity:0.5; -moz-opacity:0.5; -khtml-opacity:0.5; -webkit-opacity:0.5; filter:alpha(opacity=50);\">");
+  printf("</DIV>");
+  puts("<DIV style=\"position:absolute; left:200px; top:198px; width:240px; height:75px; z-index:100; background-color:#ffffff;
+    opacity:1; -moz-opacity:1; -khtml-opacity:1; -webkit-opacity:1; filter:alpha(opacity=100); border:1px #CCC solid;
+     box-shadow:5px 5px 20px #888888;\">");
+  puts("<table width=\"240\" height=\"70\">");
+  printf("<tr><td width=\"35px\" height=\"100%%\" align=\"center\" valign=\"middle\">\n");
+  v2 = puts("<SPAN style=\"vertical-align:middle; display:block; text-align:center;\">");
+  printf("<img src=\"/%s/apply_ani.gif\" style=\"display:inline-block; *display:inline; zoom:1; vertical-align:middle;\">", sub_14BF4(v2));
+  printf("</SPAN></td>");
+  printf("<td height=\"100%%\" align=\"center\" valign=\"middle\"><SPAN style=\"vertical-align:middle; display:block; text-align:center;\">\n");
+  printf("<p id=configmgmt_success style='line-height: 21px; height: 40px;'>");
+  printf("%s<br>%s</p>", sub_15A94(938), sub_15A94(929));
+  printf("<p id=configmgmt_fail>%s</p>", sub_15A94(937));
+  printf("<p id='configmgmt_reboot_seconds'></p>");
+  printf("</SPAN></td>");
+  return printf("</tr></table><DIV>");
 }
